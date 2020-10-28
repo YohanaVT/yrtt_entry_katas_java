25c4887ae7ccdec4d02d61106cc02717503c1341
YohanaVT <72347556+YohanaVT@users.noreply.github.com>
2020-10-28 19:41:44 +0000

    Exercise002

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,5 +1,7 @@
 package com.techreturners.exercise002;
 
+import java.time.LocalDateTime;
+
 public class Exercise002 {
 
     // The clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
@@ -7,6 +9,22 @@ public class Exercise002 {
 
     public int past(int h, int m, int s) {
         // Your code here!
-        return 0;
+    	LocalDateTime locaDate = LocalDateTime.now();
+		int hours  = locaDate.getHour();
+
+		int totalh = 0;
+    	int totalm = 0;
+    	int totals = 0;
+    	int result = 0;
+    	
+    	//You must uncomment the 'if' so that after midnight it returns in millisecond
+    	//if((hours >= 00) && (hours <= 05)) {
+    		totalh = h * 3600000;
+        	totals = s * 1000;
+        	totalm = m * 60000;
+    	//}
+    	
+    	result = totalh + totals + totalm;
+        return result;
     }
 }
