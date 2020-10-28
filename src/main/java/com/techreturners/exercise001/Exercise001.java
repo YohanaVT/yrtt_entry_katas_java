62b4bd9cf673df20b5a672f88e1052b0f9e486d5
YohanaVT <72347556+YohanaVT@users.noreply.github.com>
2020-10-28 13:21:10 +0000

    Exercise001

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,5 +1,7 @@
 package com.techreturners.exercise001;
 
+import java.util.Arrays;
+
 public class Exercise001 {
 
     // In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. 
@@ -12,6 +14,38 @@ public class Exercise001 {
 
     public int singles(int [] arr){
         // Add your code here!
-        return 0;
+    	int sum = 0;
+		int[] arrayOriginal = arr;
+		Arrays.sort(arrayOriginal);
+
+		
+		int[] arNumElementos = new int[arrayOriginal.length];
+		int contador = 0;
+		int aux = arrayOriginal[0];
+		int iterador = 0;
+		for (int i = 0; i < arrayOriginal.length; i++) {
+			if (aux == arrayOriginal[i]) {
+				contador = contador + aux;
+				if(i>=1) {
+					contador=0;
+					aux=0;
+				}
+			} else {
+				arNumElementos[iterador] = contador;
+				contador = 0;
+				aux = arrayOriginal[i];
+				contador = contador + aux;
+				iterador++;
+			}
+		}
+		arNumElementos[iterador] = contador;
+
+		for (int z = 0; z < arNumElementos.length; z++) {
+			if (arNumElementos[z] != 0) {
+				sum += arNumElementos[z];	
+			}
+
+		}
+        return sum;
     }
 }
